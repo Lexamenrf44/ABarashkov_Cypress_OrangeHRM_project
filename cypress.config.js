890b4399d9dac209d11f6dd9767ecfb7e8f5c7d0
ABarashkov <aleksander.barashkov@scompler.com>
2023-04-11 15:20:32 +0300

    configured cypress.config.js, added retries, supportFile and baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    supportFile: 'support/e2e.js',
+    baseUrl: "https://google.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
