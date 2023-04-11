70ace37f894e62582186a9ec8acb352de0f38e12
ABarashkov <aleksander.barashkov@scompler.com>
2023-04-11 15:17:33 +0300

    configured e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,20 +1,24 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import './commands.js';
+import './data.js';
+import './api/request.js';
+import 'cypress-real-events/support';
+import 'cypress-file-upload';
+import 'moment';
+import '@4tw/cypress-drag-drop'
+import registerCypressGrep from '@cypress/grep/src/support'
+registerCypressGrep()
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+//Clean state between test files (not happening automatically while testIsolation:false)
+before(() => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+})
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.on('test:after:run', (test, runnable) => {
+    // Custom code after each test
+})
+
+Cypress.on(
+    'uncaught:exception',
+    (err) => !err.message.includes('ResizeObserver loop limit exceeded')
+);
