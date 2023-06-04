907300d141b555c3df8753977f0f6681538c99d2
ABarashkov <aleksander.barashkov@scompler.com>
2023-06-04 23:23:54 +0300

    added Should logout test step

--- cypress/specs/account/authorization.cy.js
+++ cypress/specs/account/authorization.cy.js
@@ -9,6 +9,11 @@ describe('Successful authorization check', () => {
 
   it('Should login', () => {
     app.loginUI()
+    cy.wait(300)
+  })
+
+  it('Should logout', () => {
+    app.dropdownSelect(app.app_profile_dropdown, 'Logout')
   })
 
 })
